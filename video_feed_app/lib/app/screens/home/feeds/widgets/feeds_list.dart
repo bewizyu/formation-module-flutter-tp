fee0a79dcc5e5d2679a50ff969598a67d3ff7781
Hamza Hsain <hhsain@bewizyu.com>
2020-03-06 23:06:48 +0100

    TP 08 FINAL STEP

--- video_feed_app/lib/app/screens/home/feeds/widgets/feeds_list.dart
+++ video_feed_app/lib/app/screens/home/feeds/widgets/feeds_list.dart
@@ -11,9 +11,80 @@ class FeedsList extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    // TODO 1 : Afficher une liste view en utilisant ListView.builder
-    // TODO 2 : Créer un element de la listview (widgets hint : Card,  Columm, FeedsVideoPlayer, Row, Expanded, Text, InkWell, Icon, FeedsTag... )
 
-    return Container();
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Card(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              FeedsVideoPlayer(
+                url: videos[index],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(5.0),
+                      child: Text(
+                        'Lorem Iptsum',
+                        textAlign: TextAlign.left,
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      child: Icon(Icons.favorite_border),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        '250',
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  FeedsTag(
+                    label: '#Lifestyle',
+                  ),
+                  FeedsTag(
+                    label: '#Good_habits',
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500,',
+                  textAlign: TextAlign.left,
+                  style: Theme.of(context)
+                      .textTheme
+                      .display1
+                      .copyWith(fontSize: 14),
+                ),
+              ),
+            ],
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          elevation: 2,
+        );
+      },
+      itemCount: videos.length,
+    );
   }
 }
